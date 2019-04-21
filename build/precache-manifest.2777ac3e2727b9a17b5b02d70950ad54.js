03aa82a90df58130efd4703ee336fd2e23f9d720
VovikMar <vovanmar@gmail.com>
2019-04-21 21:29:59 +0300

    initial commit

---
+++ build/precache-manifest.2777ac3e2727b9a17b5b02d70950ad54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f0031696df954c6def4b",
+    "url": "/static/js/main.bd8e51c2.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f0031696df954c6def4b",
+    "url": "/static/css/main.f3493e3e.chunk.css"
+  },
+  {
+    "revision": "eddd4d85265cf14853276eb438563f4a",
+    "url": "/index.html"
+  }
+];
